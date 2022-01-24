6274e8efcf086d1267a93908f2320afdb3a9dbc7
Danila Julev <DanilJulev@gmail.com>
2022-01-24 23:41:48 +0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -2,10 +2,12 @@ $(document).ready(() => {
   $('.header__burger').click(() => {
     $('.header__burger').toggleClass('open-menu');
     $('.header__burgermenu').toggleClass('open-menu');
+    $('.body').toggleClass('lock');
   })
 
   $('.header__burgermenu-el').click(() => {
     $('.header__burger').removeClass('open-menu');
     $('.header__burgermenu').removeClass('open-menu');
+    $('.body').removeClass('lock');
   });
 });
